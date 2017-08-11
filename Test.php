7ac28d3a03a0fc238b7003937ceab9a382b8de38
szws01 <szws01@users.noreply.github.com>
2017-08-11 21:01:47 +0800

    test
    test

---
+++ Test.php
@@ -0,0 +1,15 @@
+<?php
+namespace yii\test;
+/**
+ * Created by PhpStorm.
+ * User: SuZhen
+ * Date: 2017/8/11
+ * Time: 20:39
+ */
+class Test
+{
+    public function test()
+    {
+        echo '-------';
+    }
+}
