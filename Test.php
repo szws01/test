107c567179c00999950f4a00a88922a4a87a5b4f
szws01 <szws01@users.noreply.github.com>
2017-08-14 09:37:28 +0800

    Add files via upload

---
+++ Test.php
@@ -0,0 +1,13 @@
+<?php
+namespace yii\test;
+
+class Test
+{
+    public function aaa()
+    {
+        echo '=========a';
+        //$app->on(Application::EVENT_BEFORE_REQUEST, function () {
+            // do something here
+        //});
+    }
+}
